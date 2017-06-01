8dbb438c3cebae0c82119ea4bc3b76d89f56328a
Omadzu <ophelie.delsaux@hotmail.fr>
2017-06-01 17:36:41 +0200

    Update setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='feature-oriented-sentiment-analysis',
+      version='0.1',
+      description="End-year research project about Sentiment Analysis and Deep \
+      Learning",
+      url='https://github.com/Omadzu/feature-oriented-sentiment-analysis',
+      author='Ophelie DELSAUX',
+      author_email='ophelie.delsaux@insa-lyon.fr',
+      license='MIT',
+      packages=['feature-oriented-sentiment-analysis'],
+      zip_safe=False)
